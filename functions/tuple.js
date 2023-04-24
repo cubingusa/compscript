64f0185d1249047fc81c0abd4e6d9bb0721e4eaf
Tim Reynolds <tim@cubingusa.org>
2023-04-24 07:00:52 -0400

    Rename Tuple getter methods.

--- functions/tuple.js
+++ functions/tuple.js
@@ -19,11 +19,11 @@ const Tuple = function(argCount) {
   }
 }
 
-const Get = function(argCount, selected) {
+const Get = function(name, argCount, selected) {
   const genericArgs = [...Array(argCount).keys()].map((x) => 'T' + x)
   const tupleType = 'Tuple<' + genericArgs.map((x) => '$' + x).join(', ') + '>'
   return {
-    name: 'Get' + selected,
+    name: name,
     genericParams: genericArgs,
     args: [
       {
@@ -40,7 +40,7 @@ const Get = function(argCount, selected) {
 
 module.exports = {
   functions: [
-    Tuple(1), Get(1, 1),
-    Tuple(2), Get(2, 1), Get(2, 2),
+    Tuple(1), Get('First', 1, 1),
+    Tuple(2), Get('First', 2, 1), Get('Second', 2, 2),
   ]
 }
