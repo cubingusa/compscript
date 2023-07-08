a685cfc549164f30611ba1379166640c4fa50b7b
Tim Reynolds <tim@cubingusa.org>
2023-07-08 17:22:55 -0400

    Add some utilities

--- functions/util.js
+++ functions/util.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 
 const auth = require('./../auth')
 const extension = require('./../extension')
+const lib = require('./../lib')
 
 const Type = {
   name: 'Type',
@@ -61,6 +62,75 @@ const SetExtension = {
   }
 }
 
+const RenameAssignments = {
+  name: 'RenameAssignments',
+  args: [],
+  outputType: 'String',
+  usesContext: true,
+  mutations: ['persons'],
+  implementation: (ctx) => {
+    affected = 0
+    ctx.competition.persons.forEach((person) => {
+      person.assignments.forEach((assignment) => {
+        if (assignment.assignmentCode === 'staff-announcer') {
+          affected += 1
+          assignment.assignmentCode = 'staff-Delegate'
+        }
+      })
+    })
+    return 'Updated ' + affected + ' assignments'
+  }
+}
+
+const AssignmentsBeforeCompeting = {
+  name: 'AssignmentsBeforeCompeting',
+  args: [
+    {
+      name: 'persons',
+      type: 'Array<Person>',
+    }
+  ],
+  outputType: 'Array<String>',
+  usesContext: true,
+  implementation: (ctx, persons) => {
+    var groups = lib.allGroups(ctx.competition)
+    var groupsById = Object.fromEntries(groups.map((group) => [group.wcif.id, group]))
+    var yes = {}
+    var no = {}
+    persons.forEach((person) => {
+      var assignmentEndTimes =
+        person.assignments.filter((assignment) => assignment.assignmentCode !== 'competitor')
+            .map((assignment) => groupsById[assignment.activityId])
+            .filter((group) => group !== undefined)
+            .map((group) => +group.endTime)
+      person.assignments.filter((assignment) => assignment.assignmentCode === 'competitor')
+          .map((assignment) => groupsById[assignment.activityId])
+          .filter((group) => group !== undefined)
+          .forEach((group) => {
+            var key = group.activityCode.group(null).id()
+            if (yes[key] === undefined) {
+              yes[key] = 0
+              no[key] = 0
+            }
+            if (assignmentEndTimes.includes(+group.startTime)) {
+              yes[key] += 1
+            } else {
+              no[key] += 1
+            }
+          })
+    })
+    var total = 0
+    var totalDenom = 0
+    var out = Object.entries(yes).map((e) => {
+      total += e[1]
+      totalDenom += e[1] + no[e[0]]
+      return e[0] + ': ' + e[1] + ' / ' + (e[1] + no[e[0]])
+    })
+    out.push('total: ' + total + ' / ' + totalDenom)
+    return out
+  }
+}
+
 module.exports = {
-  functions: [Type, ClearCache, SetExtension],
+  functions: [Type, ClearCache, SetExtension, RenameAssignments, AssignmentsBeforeCompeting],
 }
