2a458975fd2900c6f3463789086ebd6c90fbcfcd
Tim Reynolds <tim@cubingusa.org>
2023-07-22 09:28:20 -0400

    Printable assignments.
    Fixes #9.

--- functions/util.js
+++ functions/util.js
@@ -1,6 +1,10 @@
 const fs = require('fs')
 
+const { DateTime } = require('luxon')
+
+const activityCode = require('./../activity_code')
 const auth = require('./../auth')
+const events = require('./../events')
 const extension = require('./../extension')
 const lib = require('./../lib')
 
@@ -160,6 +164,93 @@ const CreateAssignments = {
   }
 }
 
+const AssignmentReport = {
+  name: 'AssignmentReport',
+  args: [
+    {
+      name: 'persons',
+      type: 'Array<Person>',
+    },
+    {
+      name: 'groups',
+      type: 'Array<Group>',
+    },
+    {
+      name: 'label',
+      type: 'String',
+    }
+  ],
+  outputType: 'Multi',
+  usesContext: true,
+  implementation: (ctx, persons, groups, label) => {
+    var allGroupsKeyed = Object.fromEntries(lib.allGroups(ctx.competition).map((group) => [group.wcif.id, group]))
+    var groupsByRound = {}
+    for (const group of groups) {
+      var round = group.activityCode.group(null).id()
+      if (groupsByRound[round] === undefined) {
+        groupsByRound[round] = Array()
+      }
+      groupsByRound[round].push(group)
+    }
+    var roundsSorted = Object.keys(groupsByRound).sort((roundA, roundB) => {
+      var timeA = groupsByRound[roundA][0].startTime;
+      var timeB = groupsByRound[roundB][0].startTime;
+      return timeA - timeB;
+    })
+    var maxGroupCount = 0;
+    for (const [round, groups] of Object.entries(groupsByRound)) {
+      if (groups.length > maxGroupCount) {
+        maxGroupCount = groups.length;
+      }
+    }
+    return persons.map((person) => {
+      var out = roundsSorted.map((round) => {
+        return [
+          {value: events.idToName[activityCode.parse(round).eventId]},
+          {value: groupsByRound[round][0].startTime.toLocaleString(DateTime.TIME_SIMPLE)}
+        ].concat(
+            [...Array(maxGroupCount).keys()].map((groupNumber) => {
+              if (groupNumber >= groupsByRound[round].length) {
+                return {value: ''}
+              }
+              var group = groupsByRound[round][groupNumber]
+              for (const assignment of person.assignments) {
+                var assignmentGroup = allGroupsKeyed[assignment.activityId]
+                if (!!assignmentGroup && assignmentGroup.startTime <= group.startTime && assignmentGroup.endTime > group.startTime) {
+                  var assignmentCode = assignment.assignmentCode.replace("staff-", "")
+                  var code = assignmentCode.toUpperCase()[0] + assignmentCode.substr(1) + " " + assignmentGroup.room.name.substr(0, assignmentGroup.room.name.indexOf(' '))
+                  if (assignment.stationNumber > 0) {
+                    return {value: code + " " + assignment.stationNumber}
+                  } else {
+                    return {value: code}
+                  }
+                }
+              }
+              return {value: '-'}
+            }))
+      })
+      return [
+        {
+          type: 'NoPageBreak',
+          data: [
+            {
+              type: 'Header',
+              data: person.name + ' ' + label
+            },
+            {
+              type: 'Table',
+              data: {
+                headers: ['Event', 'Time'].concat([...Array(maxGroupCount).keys()].map((x) => (x+1))),
+                rows: out,
+              }
+            },
+          ]
+        }
+      ]
+    }).flat()
+  }
+}
+
 module.exports = {
-  functions: [Type, ClearCache, SetExtension, RenameAssignments, AssignmentsBeforeCompeting, CreateAssignments],
+  functions: [Type, ClearCache, SetExtension, RenameAssignments, AssignmentsBeforeCompeting, CreateAssignments, AssignmentReport],
 }
