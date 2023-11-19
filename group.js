d3ca61a6d73ac0c4ff691b16d7c71b376923d603
Philippe Virouleau <philippe.44@gmail.com>
2023-11-19 17:23:57 +0100

    Strip Stage/Room from group name

--- group.js
+++ group.js
@@ -12,7 +12,13 @@ class Group {
 
   name() {
     if (this.activityCode.isActivity()) {
-      return this.room.name.split(' ')[0] + ' ' + this.activityCode.groupNumber
+      // This is not ideal; when displaying room names we'd like to
+      // keep only the meaningful part (eg: "Red" when it's "Red Stage", which
+      // is usually the first part of the name in English, but may be some other
+      // parts in other languages. This is a compromise where we strip out
+      // 'Stage' or 'Room' suffix, which should be enough in "regular" major
+      // competitions setup while not messing up room names in non English setups.
+      return this.room.name.replace(/Room|Stage/g, '').trim() + ' ' + this.activityCode.groupNumber
     } else {
       return this.activityCode.toString()
     }
